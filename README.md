1d10f7800a26f91a6ac16cdb6fbbeabc8ae7e96e
Henry_0005 <hrmhenrique2020@gmail.com>
2023-11-30 19:10:45 -0300

    Upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# Conversor de Ethereum
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/Henry0005/pen/LYqjraN](https://codepen.io/Henry0005/pen/LYqjraN).
+
---
+++ src/script.js
@@ -0,0 +1,6 @@
+var nomeUsuario = "Henrique";
+var valorEmEthereum = 1;
+var cotacaoEmEthereum = 9966.53;
+var valorEmReal = (valorEmEthereum * cotacaoEmEthereum).toFixed(2);
+// valorEmReal = valorEmReal.toFixed(2);
+alert('Olá ' + nomeUsuario + '! O valor em reais é de R$ '+ valorEmReal);
