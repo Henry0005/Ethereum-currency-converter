a4060dab1a25fac7f4eabdbc06bfdd98c8394a72
Henry0005 <103142324+Henry0005@users.noreply.github.com>
2023-11-30 19:27:58 -0300

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
 var nomeUsuario = "Henrique";
 var valorEmEthereum = 1;
-var cotacaoEmEthereum = 9966.53;
+var cotacaoEmEthereum = 9966.53; // Valor fixo, precisa ser atualizado manualmente
 var valorEmReal = (valorEmEthereum * cotacaoEmEthereum).toFixed(2);
 // valorEmReal = valorEmReal.toFixed(2);
 alert('Olá ' + nomeUsuario + '! O valor em reais é de R$ '+ valorEmReal);
